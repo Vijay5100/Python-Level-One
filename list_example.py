6965bf6baf06cc1823e9244e3ce873dad37cfbdb
Vijay5100 <sjonnaku@asu.edu>
2026-06-22 18:56:44 -0700

    Create list_example.py

---
+++ list_example.py
@@ -0,0 +1,14 @@
+names = ["sashi", "vijay", "jack", "jacob", "nathan"]
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[-5])
+print(names[-4])
+print(names[-3])
+print(names[-2])
+print(names[-1])
+random = [8, 9.4, True, "vijay"]
+print(random)
